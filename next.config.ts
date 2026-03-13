0b69c7d08e032139c85e251f3a5fb4bf70da70b3
rezamaun <rezamaun108@gmail.com>
2026-03-14 04:13:57 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Kita hapus output: 'export' agar API tetap hidup
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 export default nextConfig
