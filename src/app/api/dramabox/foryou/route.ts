8a615060ed89c5414e345fe7ac892e014cdc77dc
rezamaun <rezamaun108@gmail.com>
2026-03-16 10:49:45 +0800

    Update route.ts

--- src/app/api/dramabox/foryou/route.ts
+++ src/app/api/dramabox/foryou/route.ts
@@ -1,14 +1,16 @@
 export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
 
-const UPSTREAM_API = "https://api.sansekai.my.id/api/dramabox/foryou"; 
-
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
-  const voucher = searchParams.get("voucher") || ""; 
+  const voucher = searchParams.get("voucher") || "";
+  
+  const pathParts = request.nextUrl.pathname.split('/');
+  const folder = pathParts[3];
+  const UPSTREAM_API = `https://api.sansekai.my.id/api/${folder}/foryou`;
 
   try {
-    const response = await fetch(${UPSTREAM_API}?voucher=${voucher}, {
+    const response = await fetch(`${UPSTREAM_API}?voucher=${voucher}`, {
       cache: 'no-store',
     });
     const data = await response.json();
