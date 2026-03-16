4e6badd6eb1821e9c2d6da84ed8d811c62ada08a
rezamaun <rezamaun108@gmail.com>
2026-03-16 11:12:09 +0800

    Update route.ts

--- src/app/api/flickreels/detail/route.ts
+++ src/app/api/flickreels/detail/route.ts
@@ -1,17 +1,17 @@
 export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
 
-const UPSTREAM_API = "https://api.sansekai.my.id/api/flickreels/detail"; 
-
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
-  const id = searchParams.get("id");
-  const voucher = searchParams.get("voucher") || ""; 
-
-  if (!id) return NextResponse.json({ error: "ID required" }, { status: 400 });
+  const id = searchParams.get("id") || "";
+  const voucher = searchParams.get("voucher") || "";
+  
+  const pathParts = request.nextUrl.pathname.split('/');
+  const folder = pathParts[3];
+  const UPSTREAM_API = `https://api.sansekai.my.id/api/${folder}/detail`;
 
   try {
-    const response = await fetch(${UPSTREAM_API}?id=${id}&voucher=${voucher}, {
+    const response = await fetch(`${UPSTREAM_API}?id=${id}&voucher=${voucher}`, {
       cache: 'no-store',
     });
     const data = await response.json();
